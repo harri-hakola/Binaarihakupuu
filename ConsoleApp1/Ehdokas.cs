93b8a3982675cc1b9df55086eb1df6701a8da418
Harri Hakola <Harri.Hakola@seamk.fi>
2019-12-12 10:13:49 +0200

    Add project files.

---
+++ ConsoleApp1/Ehdokas.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binaarihakupuu
+{
+    public class Ehdokas
+    {
+        public string etunimi;
+        public string sukunimi;
+        public int aanimaara;
+
+
+        public Ehdokas(string etunimi, string sukunimi, int aanimaara)
+        {
+            this.etunimi = etunimi;
+            this.sukunimi = sukunimi;
+            this.aanimaara = aanimaara;
+        }
+
+        public Ehdokas leftNode;
+        public Ehdokas rightNode;
+
+        public void Insert(string etunimi, string sukunimi, int aanimaara)
+        {
+            
+            if (sukunimi.CompareTo(this.sukunimi) >= 0)
+            {
+                if (rightNode == null)
+                {
+                    rightNode = new Ehdokas(etunimi, sukunimi, aanimaara);
+                }
+                else
+                {
+                    rightNode.Insert(etunimi, sukunimi, aanimaara);
+                }
+            }
+            else
+            {
+                if (leftNode == null)
+                {
+                    leftNode = new Ehdokas(etunimi, sukunimi, aanimaara);
+                }
+                else
+                {
+                    leftNode.Insert(etunimi, sukunimi, aanimaara);
+                }
+            }
+        }
+
+        public Ehdokas Find(string value)
+        {
+            Ehdokas currentNode = this;
+
+            while (currentNode != null)
+            {
+                if (value == currentNode.sukunimi)
+                {
+                    return currentNode;
+                }
+                else if (value.CompareTo(currentNode.sukunimi) >0)
+                {
+                    currentNode = currentNode.rightNode;
+                }
+                else
+                {
+                    currentNode = currentNode.leftNode;
+                }
+            }
+            //Ei löytynyt
+            return null;
+        }
+
+        public void InOrdertraversal()
+        {
+            if (leftNode != null)
+                leftNode.InOrdertraversal();
+            Console.WriteLine(etunimi + " " + sukunimi + " " + aanimaara);
+
+            if (rightNode != null)
+                rightNode.InOrdertraversal();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Binaarihakupuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaariPuu binaariPuu = new BinaariPuu();
+            List<Ehdokas> ehdokkaat = new List<Ehdokas>();
+            string rivi;
+
+            StreamReader tiedosto = new StreamReader(@"ehdokkaat.txt", Encoding.Default);
+            while ((rivi = tiedosto.ReadLine()) != null)
+            {
+                string[] teksti = rivi.Split(new Char[] { ' ', '\t', ',' });
+                int aanet = Int32.Parse(teksti[3]);
+                
+                ehdokkaat.Add(new Ehdokas(teksti[0], teksti[1], aanet));
+            }
+            tiedosto.Close();
+
+            foreach (var item in ehdokkaat)
+            {
+                binaariPuu.Insert(item.etunimi, item.sukunimi, item.aanimaara);              
+            }
+            
+
+            Console.WriteLine("Sisäjärjestys\n");
+            binaariPuu.InOrderTraversal();
+
+            Console.WriteLine("\nAnna etsittävä sukunimi: ");
+            var node = binaariPuu.Find(Console.ReadLine());
+            Console.WriteLine(node.etunimi + " " + node.sukunimi + " " + node.aanimaara);
+
+            Console.ReadLine();
+
+        }
+    }
+}
